60cbd1bd999c68406caba58ac7c085317d2c31e9
Fran Tomljenovic <fran.tomljenovic@fer.hr>
2021-12-14 18:10:27 +0100

    Minor bugfixes

--- IzvorniKod/gunicorn_config.py
+++ IzvorniKod/gunicorn_config.py
@@ -7,7 +7,8 @@ wsgi_app = "codeshark_backend:app"
 bind = f"{cfg.get_config('flask_host')}:{cfg.get_config('flask_port')}"
 workers = mp.cpu_count() * 2
 
-if not cfg.get_config("debug"):
+if not cfg.get_config("debug_ssl"):
 	certfile = cfg.get_config("certfile")
 	keyfile = cfg.get_config("keyfile")
-	#raw_env = ["FLASK_ENV=production"]
+
+#raw_env = ["FLASK_ENV=production"]
